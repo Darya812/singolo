414011c7396963ec043da94ccbb22cfcd7891c70
Darya Radzevich <ganocij1@gmail.com>
2020-11-05 13:06:58 +0300

    fix: change content, add style , add function

--- singolo.js
+++ singolo.js
@@ -1,3 +1,78 @@
+const blockServicesNav = document.querySelector("#blockServicesNav");
+blockServicesNav.addEventListener("click", () => {
+  resetNavigationState();
+  blockServicesNav.classList.add("nav-event");
+});
+
+const blockPortfolioNav = document.querySelector("#blockPortfolioNav");
+blockPortfolioNav.addEventListener("click", () => {
+  resetNavigationState();
+  blockPortfolioNav.classList.add("nav-event");
+});
+
+const blockHomeNav = document.querySelector("#blockHomeNav");
+blockHomeNav.addEventListener("click", () => {
+  resetNavigationState();
+  blockHomeNav.classList.add("nav-event");
+});
+
+const blockAboutNav = document.querySelector("#blockAboutNav");
+blockAboutNav.addEventListener("click", () => {
+  resetNavigationState();
+  blockAboutNav.classList.add("nav-event");
+});
+
+const blockContactNav = document.querySelector("#blockContactNav");
+blockContactNav.addEventListener("click", () => {
+  resetNavigationState();
+  blockContactNav.classList.add("nav-event");
+});
+
+function resetNavigationState() {
+  blockServicesNav.classList.remove("nav-event");
+  blockPortfolioNav.classList.remove("nav-event");
+  blockContactNav.classList.remove("nav-event");
+  blockAboutNav.classList.remove("nav-event");
+  blockHomeNav.classList.remove("nav-event");
+}
+
+/*-------------portfolio tab-------------------------*/
+
+const tabPortfolioAll = document.querySelector("#tabPortfolioAll");
+tabPortfolioAll.addEventListener("click", () => {
+  resetPortfolioState();
+  tabPortfolioAll.classList.add("portfolio-event");
+});
+
+const tabPortfolioWebDesign = document.querySelector("#tabPortfolioWebDesign");
+tabPortfolioWebDesign.addEventListener("click", () => {
+  resetPortfolioState();
+  tabPortfolioWebDesign.classList.add("portfolio-event");
+});
+const tabPortfolioGraphicDesign = document.querySelector(
+  "#tabPortfolioGraphicDesign"
+);
+tabPortfolioGraphicDesign.addEventListener("click", () => {
+  resetPortfolioState();
+  tabPortfolioGraphicDesign.classList.add("portfolio-event");
+});
+const tabPortfolioArtwork = document.querySelector("#tabPortfolioArtwork");
+tabPortfolioArtwork.addEventListener("click", () => {
+  resetPortfolioState();
+  tabPortfolioArtwork.classList.add("portfolio-event");
+});
+
+function resetPortfolioState() {
+  tabPortfolioAll.classList.remove("portfolio-event");
+  tabPortfolioWebDesign.classList.remove("portfolio-event");
+  tabPortfolioGraphicDesign.classList.remove("portfolio-event");
+  tabPortfolioArtwork.classList.remove("portfolio-event");
+}
+
+
+/*------------menu burger----------------*/
+
 function openMenu() {
-    document.getElementById("sidebar").classList.toggle("active");
+    document.getElementsByClassName("header-content").classList.toggle("active");
+
 }
